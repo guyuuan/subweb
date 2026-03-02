36c2c77ed200419470e49f90bc1f671dcceabd97
guyuuan <chunjinchen1998@gmail.com>
2026-03-02 16:57:27 +0800

    feat: 增加链接,并添加cdn加速

--- public/conf/config.js
+++ public/conf/config.js
@@ -21,12 +21,76 @@ window.config = {
   // 远程配置地址,可以自行按照格式添加。
   remoteConfigOptions: [
     {
-      value: 'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini',
+      value: 'https://cdn.jsdelivr.net/gh/guyuuan/ACL4SSR@master/Clash/config/ACL4SSR_Online.ini',
       text: 'ACL4SSR Online',
     },
     {
-      value: 'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full.ini',
+      value: 'https://cdn.jsdelivr.net/gh/guyuuan/ACL4SSR@master/Clash/config/ACL4SSR_Online_Full.ini',
       text: 'ACL4SSR Online Full',
+    },
+    {
+      value: 'https://cdn.jsdelivr.net/gh/guyuuan/ACL4SSR@master/Clash/config/ACL4SSR_Online_AdblockPlus.ini',
+      text: 'ACL4SSR Online AdblockPlus',
+    },
+    {
+      value: 'https://cdn.jsdelivr.net/gh/guyuuan/ACL4SSR@master/Clash/config/ACL4SSR_Online_Full_AdblockPlus.ini',
+      text: 'ACL4SSR Online Full AdblockPlus',
+    },
+    {
+      value: 'https://cdn.jsdelivr.net/gh/guyuuan/ACL4SSR@master/Clash/config/ACL4SSR_Online_Full_Google.ini',
+      text: 'ACL4SSR Online Full Google',
+    },
+    {
+      value: 'https://cdn.jsdelivr.net/gh/guyuuan/ACL4SSR@master/Clash/config/ACL4SSR_Online_Full_MultiMode.ini',
+      text: 'ACL4SSR Online Full MultiMode',
+    },
+    {
+      value: 'https://cdn.jsdelivr.net/gh/guyuuan/ACL4SSR@master/Clash/config/ACL4SSR_Online_Full_Netflix.ini',
+      text: 'ACL4SSR Online Full Netflix',
+    },
+    {
+      value: 'https://cdn.jsdelivr.net/gh/guyuuan/ACL4SSR@master/Clash/config/ACL4SSR_Online_Full_NoAuto.ini',
+      text: 'ACL4SSR Online Full NoAuto',
+    },
+    {
+      value: 'https://cdn.jsdelivr.net/gh/guyuuan/ACL4SSR@master/Clash/config/ACL4SSR_Online_Mini.ini',
+      text: 'ACL4SSR Online Mini',
+    },
+    {
+      value: 'https://cdn.jsdelivr.net/gh/guyuuan/ACL4SSR@master/Clash/config/ACL4SSR_Online_Mini_AdblockPlus.ini',
+      text: 'ACL4SSR Online Mini AdblockPlus',
+    },
+    {
+      value: 'https://cdn.jsdelivr.net/gh/guyuuan/ACL4SSR@master/Clash/config/ACL4SSR_Online_Mini_Ai.ini',
+      text: 'ACL4SSR Online Mini Ai',
+    },
+    {
+      value: 'https://cdn.jsdelivr.net/gh/guyuuan/ACL4SSR@master/Clash/config/ACL4SSR_Online_Mini_Fallback.ini',
+      text: 'ACL4SSR Online Mini Fallback',
+    },
+    {
+      value: 'https://cdn.jsdelivr.net/gh/guyuuan/ACL4SSR@master/Clash/config/ACL4SSR_Online_Mini_MultiCountry.ini',
+      text: 'ACL4SSR Online Mini MultiCountry',
+    },
+    {
+      value: 'https://cdn.jsdelivr.net/gh/guyuuan/ACL4SSR@master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini',
+      text: 'ACL4SSR Online Mini MultiMode',
+    },
+    {
+      value: 'https://cdn.jsdelivr.net/gh/guyuuan/ACL4SSR@master/Clash/config/ACL4SSR_Online_Mini_NoAuto.ini',
+      text: 'ACL4SSR Online Mini NoAuto',
+    },
+    {
+      value: 'https://cdn.jsdelivr.net/gh/guyuuan/ACL4SSR@master/Clash/config/ACL4SSR_Online_MultiCountry.ini',
+      text: 'ACL4SSR Online MultiCountry',
+    },
+    {
+      value: 'https://cdn.jsdelivr.net/gh/guyuuan/ACL4SSR@master/Clash/config/ACL4SSR_Online_NoAuto.ini',
+      text: 'ACL4SSR Online NoAuto',
+    },
+    {
+      value: 'https://cdn.jsdelivr.net/gh/guyuuan/ACL4SSR@master/Clash/config/ACL4SSR_Online_NoReject.ini',
+      text: 'ACL4SSR Online NoReject',
     },
   ],
 };
